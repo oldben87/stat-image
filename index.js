14eae50a9b5c67637acd6e539e6b42b7876fa2ed
Ben Webb <b-webb@outlook.com>
2024-02-17 10:09:31 +0000

    Add folder to exclude from github

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer"
+import fs from "fs"
 
 const app = async ({ height = 300, width = 400 }) => {
   console.log("Making Browser")
@@ -7,7 +8,14 @@ const app = async ({ height = 300, width = 400 }) => {
   const page = await browser.newPage()
   await page.setViewport({ height, width })
   await page.setContent("<html><head><p>Howdy</p></head></html>")
-  await page.screenshot({ path: "/output/example.png" })
+
+  const path = "output"
+  try {
+    await page.screenshot({ path: `${path}/example.png` })
+  } catch {
+    fs.mkdirSync(path)
+    await page.screenshot({ path: `${path}/example.png` })
+  }
 }
 
 app({}).then(() => {
